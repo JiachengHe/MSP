36da0e52826f33593a60a6de5356dd5a15343fb8
Jiacheng He <j774h471@ku.edu>
2018-02-07 12:46:08 -0600

    Added script_2

--- MSP/script_2.R
+++ MSP/script_2.R
@@ -1,6 +1,8 @@
 devtools::load_all()
 data("BRFSS19952010")
 library(dplyr)
+library(ggplot2)
+library(lubridate)
 
 model_1 <- specification("MEDCOST", model = "DID")
 model_3 <- specification("MEDCOST", model = "event_time")
@@ -31,6 +33,36 @@ fit5 <- subgroup_regression("EDUCA %in% c('5', '6')", BRFSS19952010, model_3, cl
 
 
 
+BRFSS %>%
+  mutate(year = as.numeric(year), treated = factor(treated)) %>%
+  group_by(year, treated) %>%
+  summarize(MEDCOST = mean(MEDCOST, na.rm = TRUE)) %>%
+  ggplot(aes(year, MEDCOST, color = treated)) +
+  geom_line()
+
+paste(BRFSS$year, BRFSS$IMONTH, 1, sep="-") %>%
+  ymd() %>%
+  format("%Y-%m")
 
 
+BRFSS %>%
+  mutate(treated = factor(treated),
+         month = as.numeric(year)*12+IMONTH
+         ) %>%
+  group_by(month, treated) %>%
+  summarize(MEDCOST = mean(MEDCOST, na.rm = TRUE)) %>%
+  ggplot(aes(month, MEDCOST, color = treated)) +
+  geom_line()
 
+
+model_cps <- specification("HIMCAID", "DID")
+model_cps <- specification("HIMCAID", "event_time")
+fit_cps <- regression_analysis(CPS, model_cps, cluster="state")
+fit_cps <- regression_analysis(CPS, model_cps, cluster="state", event_plot = TRUE)
+
+CPS %>%
+  mutate(year = as.numeric(year), treated = factor(treated)) %>%
+  group_by(year, treated) %>%
+  summarize(HIMCARE = mean(HIMCARE, na.rm = TRUE)) %>%
+  ggplot(aes(year, HIMCARE, color = treated)) +
+  geom_line()
